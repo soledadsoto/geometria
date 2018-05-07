ad3f9e96355792aaece3e11a58b14a7759e42795
Sole <Sole@PC-CASA>
2018-05-07 12:28:49 +0200

    Versión comentada

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -1,14 +1,26 @@
-
+/**
+ * @author Soledad soto
+ * @version 1.2
+ *
+ */
 public class Rectangulo extends FiguraGeometrica {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * @param tipoFigura
+	 * @param lG
+	 * @param lP
+	 */
 	public Rectangulo(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
-
+/**
+ * metodo que calcula el área del rectángulo multiplicando lado mayor por el menor
+ * return area
+ */
 	@Override
 	public double area() {
 		return l1 * l2;
@@ -18,5 +30,11 @@ public class Rectangulo extends FiguraGeometrica {
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
+	
+	/**
+	 * metodo que calcula el perímetro del rectángulo con la fórmula indicada
+	 * return perimetro
+	 */
+
 
 }
